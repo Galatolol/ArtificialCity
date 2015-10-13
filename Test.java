6e01b2b7a8177d3c244e399d0cbea9506b987482
jan-klos <janklos@protonmail.com>
2015-10-13 23:42:32 +0200

    poprawki estetyczne

--- Test.java
+++ Test.java
@@ -11,11 +11,11 @@ public class Test
 			System.out.println("\n \n");
 			System.out.println(elem.getFirstName() + " " + elem.getLastName());
 			System.out.println("Wiek: " + elem.getAge());
-			System.out.println("Mieszka w: " + elem.getResidence());
-			System.out.println("Codziennie jeździe do: " + elem.getDestination());
-			System.out.println("Pochodzi z: " + elem.getFrom());
-			System.out.println("Z domu wychodzi o: " + elem.getGoingOut());
-			System.out.println("Z pracy/szkoły wraca o: " + elem.getGoingBack());
+			System.out.println("Mieszka w " + elem.getResidence());
+			System.out.println("Codziennie jeździ do " + elem.getDestination());
+			System.out.println("Pochodzi z " + elem.getFrom());
+			System.out.println("Z domu wychodzi o " + elem.getGoingOut());
+			System.out.println("Z pracy/szkoły wraca o " + elem.getGoingBack());
 		}
 	}
 }
